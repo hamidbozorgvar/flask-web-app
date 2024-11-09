8cfc1cca4b2c7e7a66d57262dcf40485c87d72dc
Hamid bozorgvar <81550698+hamidbozorgvar@users.noreply.github.com>
2024-11-09 07:52:56 +0330

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -5,8 +5,7 @@ web_app=Flask(__name__)
 @web_app.route("/")
 
 def hello():
-    #text="Hello, World! "
-    #return text
+
     return render_template("index.html")
 if __name__ == "__main__":
     web_app.run(debug=True)
